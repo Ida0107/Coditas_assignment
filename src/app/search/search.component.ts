1f521259710c3f09ac3d6f3edfc1738fcba08565
somalee gupta <somalee.gupta@gmail.com>
2018-09-12 15:11:16 +0530

    code change

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,6 +8,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  
   faSearch = faSearch;
   data : any = {};
   Users: any = null;
